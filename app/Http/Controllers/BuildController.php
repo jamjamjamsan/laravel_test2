332f2e90e9076afd5d4ed89d280c5808de9111d3
yoshimoto <kurakuenjam@aol.com>
2022-01-21 22:37:12 +0900

    commit

---
+++ app/Http/Controllers/BuildController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BuildController extends Controller
+{
+    public function index(){
+        return "建物です";
+    }
+    public function room($room){
+        return "部屋番号は".$room."です";
+    }
+}
